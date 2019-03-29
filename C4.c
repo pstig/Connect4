890d1a4be3a8f2a167a243430f8fe3c49a8e065e
pstig <pstiglitz1@gmail.com>
2019-03-29 13:55:01 -0500

    Input validation

--- C4.c
+++ C4.c
@@ -2,8 +2,8 @@
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
-
-
+#include <stdbool.h>
+#include <ctype.h>
 
 
 struct boardGraph{
@@ -15,17 +15,13 @@ struct Move{
   int column;
 };
 
-struct Edge{
-  int srcRow, srcColumn;
-  int destRow, destColumn;
-};
-
-
-// int minimax(int depth, int moveID, bool isP1, int scores[columns], int maxH);   /******Write This****************/
-// int evaluateBoard(int board[rows][columns]);                                    /******Write This****************/
-// int buildGraph(int board[rows][columns]);                                       /******Write This******Maybe?*****/
-
+/*DFS on board states to find best move*/
+int minimax(int depth, int move, bool isMax);
+/*Used to evaulate the score for a given move*/
+int positionScore(int **board);
+//Prints Board after Each Move
 void printBoard(char **board, int rows, int columns);
+//Handles adding player move to the board
 void playerMove(char **board, int *playerC, int j, int rows, int columns);
 
 int main() {
@@ -33,18 +29,32 @@ int main() {
   /*variable initialization*/
   int rows;
   int columns;
-  char *ptr;
   char **board;
-  int count = 0,i,j;
+  int i,j;
   int playerC = 0;
   int playerM;
-  int comp;
+  char temp[100];
 
   /*Get board size*/
-  printf("Enter Board height (rows)");
-  scanf("%d", &rows);
-  printf("Enter Board width (columns)");
-  scanf("%d", &columns);
+  /*All user inputs are scanned in as a string then converted to an int with atoi()
+  **that int value is then compared to the conditions nessicary for that variable*
+  **if the value is not valid it will keep asking for a valid input until one is given*/
+  printf("Enter Board height (rows): ");
+  scanf("%s", temp);
+  rows = atoi(temp);
+  while (rows <= 4){
+    printf("Please Enter a Number greater than 3: ");
+    scanf("%s", temp);
+    rows = atoi(temp);
+  }
+  printf("Enter Board width (columns): ");
+  scanf("%s", temp);
+  columns = atoi(temp);
+  while (columns <= 4){
+    printf("Please Enter a Number greater than 3: ");
+    scanf("%s", temp);
+    columns = atoi(temp);
+  }
 
   /*Allocate Board array*/
   board = malloc(rows * sizeof(char*));
@@ -58,22 +68,27 @@ int main() {
        board[i][j] = ' ';
      }
   }
+
   /*Game Start*/
-   printf("Tristan's Super Gay Connect 4! (Fixed by Peter)\n \n");
+   printf("Connect 4\n \n");
    printBoard(board, rows, columns);
-   int scores[columns];
    while(1){
      if (playerC == 0)
       printf("\n Player 1's turn. Pick a row\n");
     if (playerC == 1)
       printf("\n Player 2's turn. Pick a row\n");
-     scanf("%d", &playerM);
+    scanf("%s", temp);
+    playerM = atoi(temp);
+    while (playerM < 1 || playerM > columns){
+      printf("Invalid Move\n");
+      printf("Please choose another move: ");
+      scanf("%s", temp);
+      playerM = atoi(temp);
+    }
      playerMove(board, &playerC, playerM, rows, columns);
      printBoard(board, rows, columns);
      playerC = !(playerC);
    }
-
-
 
 
    return 0;
@@ -99,21 +114,28 @@ void playerMove(char **board, int *playerC, int j, int rows, int columns){
   while(move == 0){
     j = j - 1;
     for(int i = rows-1; i >= 0 ; i--){
-      if (board[i][j] == ' '){
-        if (*playerC == 0)
-          board[i][j] = 'X';
-        if (*playerC == 1)
-          board[i][j] = 'O';
-        return;
-      }
-      else if (board[0][j] != ' '){
+      if (board[0][j] != ' ' || j >= columns || j < 0){  //Checks to make sure number is a valid move
         printf("\nInvalid Move");
         printf("\nChoose a new move: ");
         scanf("%d", &j);
         j = j -1;
         i = rows;
       }
+      else if (board[i][j] == ' '){
+        if (*playerC == 0)
+          board[i][j] = 'X';
+        if (*playerC == 1)
+          board[i][j] = 'O';
+        return;
+      }
     }
   }
+}
 
+int minimax(int depth, int move, bool isMax){
+  return 0;
+}
+
+int positionScore(int **board){
+  return 0;
 }
